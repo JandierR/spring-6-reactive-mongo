090b5508ebb36d8ef6c2348cd44a096c77686ae8
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-05-06 09:20:24 -0600

    Adding get by id endpoint

--- src/main/java/guru/springframework/spring6reactivemongo/web/fn/BeerHandler.java
+++ src/main/java/guru/springframework/spring6reactivemongo/web/fn/BeerHandler.java
@@ -14,8 +14,15 @@ public class BeerHandler {
 
     private final BeerService beerService;
 
-    public Mono<ServerResponse> listBeers(ServerRequest serverRequest) {
-        return ServerResponse.ok()
+    public Mono<ServerResponse> getBeerById(ServerRequest request) {
+        return ServerResponse
+                .ok()
+                .body(beerService.getById(request.pathVariable("beerId")), BeerDTO.class);
+    }
+
+    public Mono<ServerResponse> listBeers(ServerRequest request) {
+        return ServerResponse
+                .ok()
                 .body(beerService.listBeers(), BeerDTO.class);
     }
 }
